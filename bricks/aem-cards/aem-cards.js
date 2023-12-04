12be5f08bd6637387acc233cedc53a24de171787
Carlos A. Cabrera <cacabrer@adobe.com>
2023-12-04 16:00:38 -0600

    cards

--- bricks/aem-cards/aem-cards.js
+++ bricks/aem-cards/aem-cards.js
@@ -1,3 +1,21 @@
 import { Brick } from '../../scripts/aem.js';
 
-export default class Cards extends Brick { }
+export default class Cards extends Brick {
+  connectedCallback() {
+    const items = [...this.root.children];
+
+    const slot = this.shadowRoot.querySelector('slot');
+
+    items.forEach((item) => {
+      const [img, text] = [...item.children];
+
+      const card = document.createElement('li');
+
+      card.innerHTML = img.innerHTML;
+
+      card.append(text);
+
+      slot.append(card);
+    });
+  }
+}
